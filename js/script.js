31ecc98d2fa7fc18c7775bd258e378655f48e9d7
carldamey <carldamey@gmail.com>
2023-09-29 15:59:18 -0700

    adds renderBoard function

--- js/script.js
+++ js/script.js
@@ -1,5 +1,9 @@
 	/*----- constants -----*/
-
+const COLORS = {
+    "0": "white",
+    "1": "orange",
+    "-1": "lightblue",
+}
 
 	/*----- state variables -----*/
 let board, turn, winner
@@ -29,5 +33,27 @@ function init() {
 }
 
 function render() {
-    
+    renderBoard()
+    renderMessage()
+    renderControls() // hide & show UI elements
+}
+
+function renderBoard() {
+    board.forEach(function(colArr, colIdx) {
+        //iterate over cells in this column
+        colArr.forEach(function(cellVal, rowIdx) {
+            const cellId = `c${colIdx}r${rowIdx}`
+            const cellEl = document.getElementById(cellId)
+            cellEl.style.backgroundColor = COLORS[cellVal]
+        })
+
+    })
+}
+
+function renderMessage() {
+
+}
+
+function renderControls() {
+
 }
