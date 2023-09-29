4411586b96c500f66b60d578ed5d6ca4547a4b57
carldamey <carldamey@gmail.com>
2023-09-29 15:19:35 -0700

    adds state variables and init() function

--- js/script.js
+++ js/script.js
@@ -2,7 +2,7 @@
 
 
 	/*----- state variables -----*/
-
+let board, turn, winner
 
 	/*----- cached elements  -----*/
 
@@ -11,3 +11,23 @@
 
 
 	/*----- functions -----*/
+init()
+
+function init() {
+    board = [
+        [0, 0, 0, 0, 0, 0,], //col 0
+        [0, 0, 0, 0, 0, 0,], //col 1
+        [0, 0, 0, 0, 0, 0,], //col 2
+        [0, 0, 0, 0, 0, 0,], //col 3
+        [0, 0, 0, 0, 0, 0,], //col 4
+        [0, 0, 0, 0, 0, 0,], //col 5
+        [0, 0, 0, 0, 0, 0,], //col 6
+    ]
+    turn = 1
+    winner = null
+    render()
+}
+
+function render() {
+    
+}
